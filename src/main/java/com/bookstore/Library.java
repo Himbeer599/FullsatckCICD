d56349b17e01f285002f95e2377de524b0a357ee
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-04 16:15:29 +0100

    Method addBook in Library

--- src/main/java/com/bookstore/Library.java
+++ src/main/java/com/bookstore/Library.java
@@ -15,6 +15,10 @@ public class Library {
                 " books=" + Arrays.toString(books) +
                 '}';
     }
+    public void add(Book newBook){
+        books = Arrays.copyOf(books,books.length+1);
+        books[books.length-1] = newBook;
+    }
 
     public static void main(String[] args) {
         Book book1 = new Book("1984", "George Orwell", "978-0451524935");
@@ -22,6 +26,9 @@ public class Library {
         Book book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565");
         Book[] books = {book1, book2, book3};
         Library library = new Library(books);
+        System.out.println(library);
+        Book book4 = new Book("Ahhh", "Max", "01234689");
+        library.add(book4);
         System.out.println(library);
     }
 }
