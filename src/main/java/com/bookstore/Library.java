4a5537b190bb03fd1f3881f2035b0b450a10c563
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-04 16:29:03 +0100

    Method addBook and deleteBook in Library (change from array to ArrayList for simplifying add/delete method)

--- src/main/java/com/bookstore/Library.java
+++ src/main/java/com/bookstore/Library.java
@@ -1,34 +1,56 @@
 package com.bookstore;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Library {
-    private Book[] books;
-    public Library(Book[] books){
-        this.books = books;
-    }
+//    private Book[] books;
+    private ArrayList<Book> books;
+//    public Library(Book[] books){
+//        this.books = books;
+//    }
+public Library(ArrayList<Book> books){
+    this.books = books;
+}
 
     @Override
     public String toString() {
         return "Library{" +
-                " books=" + Arrays.toString(books) +
+                "books=" + books +
                 '}';
     }
+
+    //    @Override
+//    public String toString() {
+//        return "Library{" +
+//                " books=" + Arrays.toString(books) +
+//                '}';
+//    }
+//    public void add(Book newBook){
+//        books = Arrays.copyOf(books,books.length+1);
+//        books[books.length-1] = newBook;
+//    }
     public void add(Book newBook){
-        books = Arrays.copyOf(books,books.length+1);
-        books[books.length-1] = newBook;
+    books.add(newBook);
+    }
+    public void delete(Book deleteBook){
+    books.remove(deleteBook);
     }
 
     public static void main(String[] args) {
         Book book1 = new Book("1984", "George Orwell", "978-0451524935");
         Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084");
         Book book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565");
-        Book[] books = {book1, book2, book3};
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
         Library library = new Library(books);
         System.out.println(library);
         Book book4 = new Book("Ahhh", "Max", "01234689");
         library.add(book4);
         System.out.println(library);
+        System.out.println("delete book: "+book1);
     }
 }
