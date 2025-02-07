6cbf9ab96128850bdf27e975a319d244f3d1d005
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-07 10:45:13 +0100

    List(example of students in school)

---
+++ src/main/java/com/LSM/Main.java
@@ -0,0 +1,21 @@
+package com.LSM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        School school = new School();
+        Student student1 = new Student("Joy", "Pilson", 1);
+        Student student2 = new Student("Kevin", "Lovn", 2);
+        Student student3 = new Student("Mike", "Penson", 3);
+        Student student4 = new Student("Jack", "Milson", 4);
+        Student student5 = new Student("Alicia", "Rand", 5);
+        school.addStudent(student1);
+        school.addStudent(student2);
+        school.addStudent(student3);
+        school.addStudent(student4);
+        school.addStudent(student5);
+        school.printAllStudents();
+    }
+}
---
+++ src/main/java/com/LSM/School.java
@@ -0,0 +1,27 @@
+package com.LSM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<Student> students;
+
+    public School() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void printAllStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students in the school");
+        } else {
+            System.out.println("List of all students:");
+            for (Student student : students) {
+                System.out.println(student);
+            }
+        }
+    }
+}
