9cc7a08ee7c60ed04a04aa849128f2c6430cf072
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-10 16:39:16 +0100

    test all methods in ProductRepo

--- src/test/java/com/recapProjekt/ProductRepoTest.java
+++ src/test/java/com/recapProjekt/ProductRepoTest.java
@@ -2,6 +2,7 @@ package com.recapProjekt;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,10 +21,30 @@ class ProductRepoTest {
     }
 
     @Test
-    void removeProducts() {
+    void expectedfalse_removeproduct() {
+        Product product = new Product("1","Laptop",1200);
+        productRepo.removeProducts(product);
+        List<Product> products = productRepo.getProducts();
+        assertFalse(products.contains(product),"Product should not be in the list");
+
     }
 
     @Test
     void getProduct() {
+        Product product = new Product("1","Laptop",1200);
+        productRepo.addProducts(product);
+        Product exptected = productRepo.getProduct("1");
+        assertEquals(exptected,product);
+        Product notexisted = productRepo.getProduct("45");
+        assertNull(notexisted,"Product should not be found");
     }
+
+//    public Product getProduct(String id){
+//        for(Product product:products){
+//            if(product.id() == id){
+//                return product;
+//            }
+//        }
+//        return null;
+//    }
 }
