e3bcae3b4256af22a4a998dd0a69a432423021f8
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-10 14:28:38 +0100

    Create a class OrderListRepo that contains add,remove and get order/orders

---
+++ src/main/java/com/recapProjekt/Order.java
@@ -0,0 +1,4 @@
+package com.recapProjekt;
+
+public record Order(String id, String username, String productname, int count, double price) {
+}
---
+++ src/main/java/com/recapProjekt/OrderListRepo.java
@@ -0,0 +1,36 @@
+package com.recapProjekt;
+
+import java.util.List;
+
+public class OrderListRepo {
+    private List<Order> orders;
+
+    public OrderListRepo(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public void removeOrder(Order order) {
+        orders.remove(order);
+    }
+
+    public Order getOrderById(String id) {
+        for (Order order : orders) {
+            if (order.id().equals(id)) {
+                return order;
+            }
+        }
+        return null;
+    }
+}
