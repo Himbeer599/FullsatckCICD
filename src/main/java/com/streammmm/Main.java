3e5c16aecb920e5df16c36501d729a043e05aed3
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-12 15:36:28 +0100

    rename new collect list

--- src/main/java/com/streammmm/Main.java
+++ src/main/java/com/streammmm/Main.java
@@ -8,19 +8,19 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        List<Integer> newNumbers = numbers.stream()
+        List<Integer> collect = numbers.stream()
                 .filter(number->number% 2 ==0)
                 .map(number->number*2)
                 .sorted()
                 .collect(Collectors.toList());
 
-        System.out.println(newNumbers);
+        System.out.println(collect);
 
-        int sum = newNumbers.stream()
+        int sum = collect.stream()
                 .reduce(0,Integer::sum);
-        newNumbers.forEach(System.out::println);
+        collect.forEach(System.out::println);
         System.out.println("Sum of processed numbers: " +sum);
-        System.out.println("Collected List: "+newNumbers);
+        System.out.println("Collected List: "+collect);
 
     }
 }
